4cac16b3b07696d043186f1fd28a0ede721787b8
Josue Hernandez <josue.hernandez@mercadolibre.cl>
2017-10-31 11:49:37 -0300

    Added a new view to the Bottom.

--- ConstraintProject/ViewController.swift
+++ ConstraintProject/ViewController.swift
@@ -20,17 +20,27 @@ class ViewController: UIViewController {
         //Create a new UIView
         let my_view = UIView();
         
+        //Create a green View
+        let green_view = UIView();
+        
         //set a background color
         my_view.backgroundColor = UIColor.orange;
+        
+        //Set background color to the Green View.
+        green_view.backgroundColor = UIColor.green;
         
         //Add the new View (view) into ContentView (IBLoulet)
         self.contentView.addSubview(my_view);
         
+        //Add green view to contentView
+        self.contentView.addSubview(green_view);
+        
         //Remove constratints
-        my_view.translatesAutoresizingMaskIntoConstraints = false
+        my_view.translatesAutoresizingMaskIntoConstraints = false;
+        green_view.translatesAutoresizingMaskIntoConstraints = false;
         
         //build dinctionary of views
-        let viewsDict = ["my_view": my_view];
+        let viewsDict = ["my_view": my_view,"green_view":green_view];
         
         //Horizontal constraints
         let horizontalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "H:|[my_view]|", options: [], metrics: nil, views: viewsDict)
@@ -39,6 +49,15 @@ class ViewController: UIViewController {
         //Vertical constraints
         let verticalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "V:|[my_view]|", options: [], metrics: nil, views: viewsDict)
         self.contentView.addConstraints(verticalConstraints)
+        
+        //Constraint Vrtical to the green_view
+        let verticalConstraintsOrange = NSLayoutConstraint.constraints(withVisualFormat: "V:|[my_view]-10-[green_view(40)]|", options: [], metrics: nil, views: viewsDict);
+        self.contentView.addConstraints(verticalConstraintsOrange);
+        
+        // Contraint Horizontal to the green_view
+        let horizontalConstraintsGreen = NSLayoutConstraint.constraints(withVisualFormat: "H:|[green_view]|", options: [], metrics: nil, views: viewsDict)
+        self.contentView.addConstraints(horizontalConstraintsGreen)
+        
         
     }
 
