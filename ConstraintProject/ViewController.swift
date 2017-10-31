067cda28100fa5bfa109e22674feded089e7ac69
Josue Hernandez <josue.hernandez@mercadolibre.cl>
2017-10-31 00:33:29 -0300

    Initial Commit.

--- ConstraintProject/ViewController.swift
+++ ConstraintProject/ViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var contentView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Documentation
+        //https://code.tutsplus.com/tutorials/introduction-to-the-visual-format-language--cms-22715
+        
+        //Create a new UIView
+        let my_view = UIView();
+        
+        //set a background color
+        my_view.backgroundColor = UIColor.orange;
+        
+        //Add the new View (view) into ContentView (IBLoulet)
+        self.contentView.addSubview(my_view);
+        
+        //Remove constratints
+        my_view.translatesAutoresizingMaskIntoConstraints = false
+        
+        //build dinctionary of views
+        let viewsDict = ["my_view": my_view];
+        
+        //Horizontal constraints
+        let horizontalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "H:|[my_view]|", options: [], metrics: nil, views: viewsDict)
+        self.contentView.addConstraints(horizontalConstraints)
+        
+        //Vertical constraints
+        let verticalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "V:|[my_view]|", options: [], metrics: nil, views: viewsDict)
+        self.contentView.addConstraints(verticalConstraints)
+        
     }
 
     override func didReceiveMemoryWarning() {
